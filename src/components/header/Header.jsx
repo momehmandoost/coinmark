4f29a57d21830e209761780851d91a3b12bdac5d
mohammad <mehmnadoost2@gmail.com>
2023-07-17 19:50:22 +0430

    add hamburger menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import React from "react";
 import "./header.css";
 
 export const Header = () => {
+  const [click, setClick] = useState(false);
   return (
     <div className="navbar">
       <div className="logo">COINMARK</div>
@@ -25,7 +27,33 @@ export const Header = () => {
         <span>
           <i class="fa-brands fa-twitter"></i>
           <i class="fa-brands fa-discord"></i>
-          <i class="fa-solid fa-bars-staggered hamburger-menu"></i>
+
+          <div className="hamburger" onClick={() => setClick(!click)}>
+            {click ? (
+              <i class="fa-solid fa-xmark close-mobile"></i>
+            ) : (
+              <i class="fa-solid fa-bars-staggered hamburger-menu"></i>
+            )}
+            {click ? (
+              <div className="sidemenu">
+                <ul>
+                  <li>
+                    <a href="#home">Home</a>
+                  </li>
+                  <li>
+                    <a href="#about">About</a>
+                  </li>
+                  <li>
+                    <a href="#projects">Projects</a>
+                  </li>
+                  <li>
+                    <a href="#contact">Contact</a>
+                  </li>
+                  <li></li>
+                </ul>
+              </div>
+            ) : null}
+          </div>
         </span>
       </div>
     </div>
